20fc3cafe7467719a9cd7439a5c0f98df9259a94
Nat-Serrano <naticio@gmail.com>
2022-07-29 18:27:24 -0500

    New version 2.0

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  //content: ["./public//*.html", "./src//*.{js,jsx,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  //enabled: process.env.NODE_ENV === "production",
+};
